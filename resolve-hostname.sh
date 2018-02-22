890808996d42ceb5714d697f041a98ca455a4208
ricardojohnny <31898079+ricardojohnny@users.noreply.github.com>
2018-02-22 10:44:18 -0300

    Update

--- resolve-hostname.sh
+++ resolve-hostname.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-# Starting new instance in VPC often results that `hostname` returns something like 'ip-10-1-1-24', which is
-# not resolvable. Which leads to problems like SparkUI failing to bind itself on start up to that hostname as
-# described in https://issues.apache.org/jira/browse/SPARK-5246.
-# This script maps private ip to such hostname via '/etc/hosts'.
-#
-
-# Are we in VPC?
-MAC=`wget -q -O - http://169.254.169.254/latest/meta-data/mac`
-VCP_ID=`wget -q -O - http://169.254.169.254/latest/meta-data/network/interfaces/macs/${MAC}/vpc-id`
-if [ -z "${VCP_ID}" ]; then
-    # echo "nothing to do - instance is not in VPC"
-=======
 # Iniciando uma nova instância no VPC geralmente resulta que `hostname' retornam algo como ' ip-10-1-1-24', que por sua vez é
 # não resolvivel. O que leva a problemas com o SparkUI que não starta ao iniciar com esse nome de host
 # Este script mapeia ip privado para esse nome de host via '/etc/hosts'.
@@ -21,7 +8,6 @@ if [ -z "${VCP_ID}" ]; then
 MAC=`wget -q -O - http://169.254.169.254/latest/meta-data/mac`
 VCP_ID=`wget -q -O - http://169.254.169.254/latest/meta-data/network/interfaces/macs/${MAC}/vpc-id`
 if [ -z "${VCP_ID}" ]; then
->>>>>>> origin/master
     exit 0
 fi
 
@@ -29,18 +15,6 @@ SHORT_HOSTNAME=`hostname`
 
 PRIVATE_IP=`wget -q -O - http://169.254.169.254/latest/meta-data/local-ipv4`
 
-<<<<<<< HEAD
-# do changes only if short hostname does not resolve
-ping -c 1 -q "${SHORT_HOSTNAME}" > /dev/null 2>&1
-if [ $? -ne 0  ]; then
-    echo -e "\n# fixed by resolve-hostname.sh \n${PRIVATE_IP} ${SHORT_HOSTNAME}\n" >> /etc/hosts
-
-    # let's make sure that it got fixed
-    ping -c 1 -q "${SHORT_HOSTNAME}" > /dev/null 2>&1
-    if [ $? -ne 0 ]; then
-        # return some non-zero code to indicate problem
-        echo "Possible bug: unable to fix resolution of local hostname"
-=======
 ping -c 1 -q "${SHORT_HOSTNAME}" > /dev/null 2>&1
 if [ $? -ne 0  ]; then
     echo -e "\n# fixado no resolve-hostname.sh \n${PRIVATE_IP} ${SHORT_HOSTNAME}\n" >> /etc/hosts
@@ -49,7 +23,6 @@ if [ $? -ne 0  ]; then
     if [ $? -ne 0 ]; then
         # returno de problemas
         echo "Possivel bug: nao consegue corrigir a resolucao do nome de host local (script: resolve-hostname.sh)"
->>>>>>> origin/master
         return 62
     fi
 
