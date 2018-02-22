aaf9a97b1941fc6ebb2281743ec98ab19e0b67e8
ricardojohnny <31898079+ricardojohnny@users.noreply.github.com>
2018-02-22 17:11:57 -0300

    Remove JVM7

--- create_image.sh
+++ create_image.sh
@@ -10,7 +10,7 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 # Dev tools
-sudo yum remove java-*
+sudo yum remove java-1.7.0-openjdk
 sudo yum install -y java-1.8.0-openjdk java-1.8.0-openjdk-devel gcc gcc-c++ ant git
 
 # Perf tools
