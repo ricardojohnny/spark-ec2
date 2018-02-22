87ca5691beaf2f1ec40f9748c9a4682bc8db2c9d
ricardojohnny <31898079+ricardojohnny@users.noreply.github.com>
2018-02-22 11:32:37 -0300

    Update

--- ssh-no-keychecking.sh
+++ ssh-no-keychecking.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-<<<<<<< HEAD
-# Utility script that exec's SSH without key checking so that we can check
-# out code from GitHub without prompting the user.
-=======
 # Verifica via SSH a Key do projeto
->>>>>>> origin/master
-
 exec ssh -o StrictHostKeyChecking=no $@
