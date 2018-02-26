acf3d45e619f00278dc9c5d189c5f21bc3ec8bb7
Ricardo Johnny <ricardo.johnny.j@gmail.com>
2018-02-26 11:44:08 -0300

    Adjust script Delivery

--- jobserver/setup.sh
+++ jobserver/setup.sh
@@ -2,11 +2,19 @@
 # Arquitetura Cloud Wealthsystems
 
 # Permission files
+echo -e "....: Setando as permissoes nos arquivos :....\n"
 chmod a+x /root/jobserver/server_start.sh
 chmod a+x /root/jobserver/server_stop.sh
+echo -e "....: permissoes setadas! :....\n"
 
 # Time do Job
+echo -e "....: Setando as permissoes nos arquivos :....\n"
 sleep 90
 
+
 # Start jobserver
-./root/jobserver/server_start.sh
+./jobserver/server_start.sh
+
+sleep 10
+
+cat /var/log/job-server/spark-job-server.log
