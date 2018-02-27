dffe165a6ceb08e8524a974cc3e438a0d6565611
Ricardo Johnny <ricardo.johnny.j@gmail.com>
2018-02-27 18:14:01 -0300

    Add config H2 Base

--- H2/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Download H2
-wget --no-check-certificate https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/h2database/h2-2012-11-30.zip -P /tmp
-
-# Unpacking H2 in /root
-unzip /tmp/h2-2012-11-30.zip -d /root
- 
