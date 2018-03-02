072b38582266cc7b5cb511c030ddbf8c06e61811
Ricardo Johnny <ricardo.johnny.j@gmail.com>
2018-03-02 15:33:02 -0300

    Update Patch Spark ed2

--- spark/init.sh
+++ spark/init.sh
@@ -40,7 +40,7 @@ else
   echo "Unpacking Spark"
   tar xvzf spark-*.tgz > /tmp/spark-ec2_spark.log
   rm spark-*.tgz
-  mv `ls -d spark-* | grep -v ec2` spark
+  mv -f `ls -d spark-* | grep -v ec2` spark
 fi
 
 popd > /dev/null
